d69b678d9237ca639ed6467ab8a601d635766b9f
ngongang <k.ngongang@addon-solution.de>
2024-07-05 13:47:32 +0200

    chore: Update travel_recommendation.html, travel_recommendation.js, header.html, contact.html, and index.html

---
+++ travel_recommendation.js
@@ -0,0 +1,15 @@
+
+
+ document.addEventListener("DOMContentLoaded", function () {
+   const includeHTML = (el, url) => {
+     fetch(url)
+       .then((response) => response.text())
+       .then((data) => {
+         el.innerHTML = data;
+       })
+       .catch((err) => console.log(err));
+   };
+
+   includeHTML(document.getElementById("header"), "header.html");
+   includeHTML(document.getElementById("aside"), "aside.html");
+ });
